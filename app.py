44475f539fc8b26ca864a776045b26c3f9bd28b2
manya <manyabuzz85@gmail.com>
2024-03-17 23:29:07 +0530

    your

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd 
-import matplotlib.pyplot as plt
 from plotly import graph_objs as go
 from scipy.stats import norm 
 from sklearn.linear_model import LinearRegression
